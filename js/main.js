8c3c10240bfd4c919622796628c643e22ec4f5d6
Vov2002 <Vladimiy2002@mail.ru>
2023-02-06 18:23:48 +0300

    DZ5

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//Задание 1
+for(i = 1;i <= 50;i++){
+    console.log(i)
+}
+//Задание 2
+for(i = 50; i >= 25;i--){
+    console.log(i)
+}
+//Задание 3
+for(i = 2500; i < 3000; i++){
+    if(i % 2 ==0)continue
+    console.log(i)
+}
